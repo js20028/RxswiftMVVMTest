01c23dac8af2fbd06879f9eceeafa37c8461fe0c
js20028 <mix2088@nate.com>
2022-12-29 20:44:18 +0900

    Initial Commit

---
+++ RxswiftMVVMTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxswiftMVVMTest
+//
+//  Created by 곽재선 on 2022/12/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
